53dfc4e3bbd75f6f7e12405a9808e741126fd9fa
Ugochukwu Amajor <maximmajor4us@gmail.com>
2023-11-14 04:54:41 +0100

    seting up firebase and enivironment

--- password-manager/src/app/app.module.ts
+++ password-manager/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"password-manager-2ac40","appId":"1:763811871926:web:84d561b3cf015c62007a17","storageBucket":"password-manager-2ac40.appspot.com","apiKey":"AIzaSyAagymXc8AVJHXK3Fz-WpGg5iMdLZ_AgEc","authDomain":"password-manager-2ac40.firebaseapp.com","messagingSenderId":"763811871926"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
